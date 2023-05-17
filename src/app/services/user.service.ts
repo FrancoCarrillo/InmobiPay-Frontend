1e4d6222145b5f928b50209661551ebacd889256
FrancoCarrillo <u202013943@upc.edu.pe>
2023-05-17 17:24:03 -0500

    feat: :sparkles: add enviroments

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from './../../environment/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private apiUrl = `${environment.API_URL}/users`;
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  create(dto: string) {
+    return this.http.post<string>(this.apiUrl, dto);
+  }
+
+  getAll() {
+    return this.http.get<string[]>(this.apiUrl);
+  }
+}
