de561efbf2e58e46f4762f6392f73b6d9fd81b9c
FrancoCarrillo <u202013943@upc.edu.pe>
2023-06-20 18:09:30 -0500

    feat: :sparkles: add finalbalance column to schedule

--- src/app/core/models/entity/loanForm.model.ts
+++ src/app/core/models/entity/loanForm.model.ts
@@ -1,17 +1,17 @@
-
 export interface LoanForm {
-  rate:               number;
-  amountPayments:     number;
-  propertyValue:      number;
-  loanAmount:         number;
-  lienInsurance:      number;
-  allRiskInsurance:   number;
+  rate: number;
+  amountPayments: number;
+  propertyValue: number;
+  loanAmount: number;
+  lienInsurance: number;
+  allRiskInsurance: number;
   administrativeExpenses: number;
   postage: number;
   commissions: number;
   currencyName: number;
-  interestRateType:   string;
-  isGoodPayerBonus:   boolean;
-  isGreenBonus:       boolean;
-  cokRate:            number;
+  interestRateType: string;
+  isGoodPayerBonus: boolean;
+  isGreenBonus: boolean;
+  cokRate: number;
+  finalBalance: number;
 }
