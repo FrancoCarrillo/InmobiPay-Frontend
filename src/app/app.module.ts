71e016c5b5f1e173bfabe9fcf28b974ec9d779ef
FrancoCarrillo <u202013943@upc.edu.pe>
2023-06-20 16:35:39 -0500

    feat: :sparkles: improve login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { AuthInterceptor } from './core/interceptor/auth.interceptor';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { ReactiveFormsModule } from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
 
 import {MatButtonModule} from '@angular/material/button';
 @NgModule({
@@ -25,6 +26,7 @@ import {MatButtonModule} from '@angular/material/button';
     MatFormFieldModule,
     ReactiveFormsModule,
     MatInputModule,
+    MatIconModule,
     MatButtonModule
     
     
